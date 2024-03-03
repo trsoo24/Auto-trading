7d9e500513ffa32396782776c5fcaa7abc081779
trsoo24 <trsoo24@naver.com>
2024-03-03 19:01:44 +0900

    feat : 에러 코드 추가

--- src/main/java/com/project/exception/ErrorCode.java
+++ src/main/java/com/project/exception/ErrorCode.java
@@ -16,10 +16,19 @@ public enum ErrorCode {
     // WebSocket
     WEBSOCKET_CLOSED(HttpStatus.BAD_REQUEST, "서버가 닫혀있습니다."),
 
-
     // Market
     MARKET_NOT_FOUND(HttpStatus.NOT_FOUND, "가상 화폐를 찾을 수 없습니다."),
-    INCORRECT_SEARCH_METHOD(HttpStatus.BAD_REQUEST, "올바르지 않은 검색 방법입니다.")
+    INCORRECT_SEARCH_METHOD(HttpStatus.BAD_REQUEST, "올바르지 않은 검색 방법입니다."),
+
+    // Wallet
+    INVALID_WALLET(HttpStatus.BAD_REQUEST, "유효하지 않은 지갑 주소입니다."),
+
+    // Portfolio
+    NOT_OWN_CURRENCY(HttpStatus.BAD_REQUEST, "보유하지 않은 가상 화폐입니다"),
+
+    // Trade
+    NOT_ENOUGH_CASH(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+    NOT_ENOUGH_VOLUME(HttpStatus.BAD_REQUEST, "수량이 부족합니다.")
     ;
     private final HttpStatus httpStatus;
     private final String detail;
