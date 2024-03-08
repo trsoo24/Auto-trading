af6537a4821c3bc0f92023804c8c1a6d1dbe9192
trsoo24 <trsoo24@naver.com>
2024-03-08 16:57:23 +0900

    fix : EC2 서버 내 순환 참조 해결

--- src/main/java/com/project/reference/CheckWalletReference.java
+++ src/main/java/com/project/reference/CheckWalletReference.java
@@ -4,7 +4,6 @@ import com.project.exception.CustomException;
 import com.project.user.entity.User;
 import com.project.wallet.entity.Wallet;
 import com.project.wallet.repository.WalletRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +13,9 @@ import static com.project.exception.ErrorCode.INVALID_WALLET;
 @RequiredArgsConstructor
 public class CheckWalletReference {
     private final WalletRepository walletRepository;
-    private final CheckUserReference checkUserReference;
 
     public Wallet findWalletByUser(User user) {
         return walletRepository.findByUser(user)
                 .orElseThrow(() -> new CustomException(INVALID_WALLET));
-    }
-
-    public Wallet findWalletByRequestHeader(HttpServletRequest request) {
-        String email = checkUserReference.checkUserReference(request);
-
-        User user = checkUserReference.findUserByEmail(email);
-
-        return findWalletByUser(user);
     }
 }
