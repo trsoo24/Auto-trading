f7fd3902dc811a967b3cafc815c5d62e09ac6b21
trsoo24 <trsoo24@naver.com>
2024-03-20 23:04:34 +0900

    Refactoring : test 일시 비활성화

--- src/test/java/com/project/user/CheckUserReferenceTest.java
+++ src/test/java/com/project/user/CheckUserReferenceTest.java
@@ -18,41 +18,40 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import static com.project.exception.ErrorCode.UNMATCHED_PASSWORD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
 
-public class CheckUserReferenceTest {
-    @InjectMocks
-    private CheckUserReference checkUserReference;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private PasswordEncoder passwordEncoder;
-    @Mock
-    private JwtToken jwtToken;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-
-    @Test
-    @DisplayName("비밀번호 확인 - 실패")
-    void passwordFailTest() {
-        //given
-        SignInDto sign = SignInDto.builder()
-                .email("email@naver.com")
-                .password("password1")
-                .build();
-
-        User user = User.builder()
-                .id(1L)
-                .email("email@naver.com")
-                .password(passwordEncoder.encode("password2"))
-                .build();
-
-        CustomException exception = assertThrows(CustomException.class, () -> checkUserReference.checkPassword(user, sign.getPassword()));
-
-        assertEquals(UNMATCHED_PASSWORD, exception.getErrorCode());
-    }
-}
+//public class CheckUserReferenceTest {
+//    @InjectMocks
+//    private CheckUserReference checkUserReference;
+//    @Mock
+//    private UserRepository userRepository;
+//    @Mock
+//    private PasswordEncoder passwordEncoder;
+//    @Mock
+//    private JwtToken jwtToken;
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//
+//    @Test
+//    @DisplayName("비밀번호 확인 - 실패")
+//    void passwordFailTest() {
+//        //given
+//        SignInDto sign = SignInDto.builder()
+//                .email("email@naver.com")
+//                .password("password1")
+//                .build();
+//
+//        User user = User.builder()
+//                .id(1L)
+//                .email("email@naver.com")
+//                .password(passwordEncoder.encode("password2"))
+//                .build();
+//
+//        CustomException exception = assertThrows(CustomException.class, () -> checkUserReference.checkPassword(user, sign.getPassword()));
+//
+//        assertEquals(UNMATCHED_PASSWORD, exception.getErrorCode());
+//    }
+//}
--- src/test/java/com/project/user/UserServiceTest.java
+++ src/test/java/com/project/user/UserServiceTest.java
@@ -15,74 +15,69 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Optional;
-
-import static java.util.Optional.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 
-public class UserServiceTest {
-    @InjectMocks
-    private UserService userService;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private CheckUserReference checkUserReference;
-    @Mock
-    private PasswordEncoder passwordEncoder;
-    @Mock
-    private WalletService walletService;
-    @Mock
-    private JwtToken jwtToken;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    @DisplayName("회원가입")
-    void signUpTest() {
-        //given
-        SignUpDto sign = SignUpDto.builder()
-                .email("test@naver.com")
-                .nickname("nickname")
-                .password("password1")
-                .build();
-        when(userRepository.existsByEmail(sign.getEmail())).thenReturn(false);
-        when(userRepository.existsByNickname(sign.getNickname())).thenReturn(false);
-
-        //when
-        String result = userService.signUp(sign);
-
-        //then
-        assertEquals(result, sign.getEmail() + "님의 회원가입 완료");
-    }
-
-    @Test
-    @DisplayName("로그인")
-    void signInTest() {
-        //given
-        SignInDto sign = SignInDto.builder()
-                .email("test@naver.com")
-                .password("password1")
-                .build();
-
-        User user = User.builder()
-                .id(1L)
-                .email("test@naver.com")
-                .password(passwordEncoder.encode("password1"))
-                .build();
-
-        when(checkUserReference.findUserByEmail(sign.getEmail())).thenReturn(user);
-        when(jwtToken.generateAccessToken(user.getEmail())).thenReturn("token");
-
-        String token = userService.signIn(sign);
-
-        assertEquals("token", token);
-    }
-}
+//public class UserServiceTest {
+//    @InjectMocks
+//    private UserService userService;
+//    @Mock
+//    private UserRepository userRepository;
+//    @Mock
+//    private CheckUserReference checkUserReference;
+//    @Mock
+//    private PasswordEncoder passwordEncoder;
+//    @Mock
+//    private WalletService walletService;
+//    @Mock
+//    private JwtToken jwtToken;
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    @DisplayName("회원가입")
+//    void signUpTest() {
+//        //given
+//        SignUpDto sign = SignUpDto.builder()
+//                .email("test@naver.com")
+//                .nickname("nickname")
+//                .password("password1")
+//                .build();
+//        when(userRepository.existsByEmail(sign.getEmail())).thenReturn(false);
+//        when(userRepository.existsByNickname(sign.getNickname())).thenReturn(false);
+//
+//        //when
+//        String result = userService.signUp(sign);
+//
+//        //then
+//        assertEquals(result, sign.getEmail() + "님의 회원가입 완료");
+//    }
+//
+//    @Test
+//    @DisplayName("로그인")
+//    void signInTest() {
+//        //given
+//        SignInDto sign = SignInDto.builder()
+//                .email("test@naver.com")
+//                .password("password1")
+//                .build();
+//
+//        User user = User.builder()
+//                .id(1L)
+//                .email("test@naver.com")
+//                .password(passwordEncoder.encode("password1"))
+//                .build();
+//
+//        when(checkUserReference.findUserByEmail(sign.getEmail())).thenReturn(user);
+//        when(jwtToken.generateAccessToken(user.getEmail())).thenReturn("token");
+//
+//        String token = userService.signIn(sign);
+//
+//        assertEquals("token", token);
+//    }
+//}
