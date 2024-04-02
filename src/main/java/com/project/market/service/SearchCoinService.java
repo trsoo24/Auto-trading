0875faa36e81b0d45da6753b0dabfaea5be3d0c5
trsoo24 <trsoo24@naver.com>
2024-04-02 16:30:52 +0900

    Fix : 클래스 명칭 변경

--- src/main/java/com/project/market/service/SearchCoinService.java
+++ src/main/java/com/project/market/service/SearchCoinService.java
@@ -2,7 +2,7 @@ package com.project.market.service;
 
 import com.project.market.entity.Market;
 import com.project.market.repository.MarketRepository;
-import com.project.websocket.model.JsonDto;
+import com.project.websocket.model.WebSocketJsonDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class CoinService {
+public class SearchCoinService {
     private final MarketRepository marketRepository;
 
     public Page<Market> pageAllMarket() {
@@ -33,8 +33,8 @@ public class CoinService {
         return marketNameToString;
     }
 
-    public JsonDto searchTicker(List<String> marketNameList) {
-        JsonDto jsonDto = new JsonDto();
+    public WebSocketJsonDto searchTicker(List<String> marketNameList) {
+        WebSocketJsonDto jsonDto = new WebSocketJsonDto();
         jsonDto.searchMarketTicker(marketNameList);
 
         return jsonDto;
